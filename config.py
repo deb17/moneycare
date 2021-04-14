21533f402d3334bf6e51aca7ca698fece360e083
Debashish Palit <dpalit07@gmail.com>
2021-04-14 16:25:26 +0530

    Minor css changes for mobile

--- config.py
+++ config.py
@@ -28,3 +28,9 @@ class Config:
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
     FLASK_ADMIN_SWATCH = 'cerulean'
+
+
+class TestingConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    WHOOSH_INDEX_PATH = '.indexes-test'
